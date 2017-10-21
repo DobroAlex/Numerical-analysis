2edb193b301fdd9a61d6acf12cd6c314bb4c92f1
Alex <alexkoshernosiegov@gmail.com>
2017-10-21 13:50:39 +0400

    Добавлен генератор  случайных вещественных

--- NumericalMethodsMath.cpp
+++ NumericalMethodsMath.cpp
@@ -1,8 +1,9 @@
-#include "stdafx.h"
+/*TODO : Ð¿Ñ€Ð¸Ð²ÐµÑÑ‚Ð¸ Ð²ÑÑ‘ Ðº Ð¾Ð´Ð½Ð¾Ð¹ ÐºÐ¾Ð´Ð¸Ñ€Ð¾Ð²ÐºÐµ. MVS Ð´ÐµÐ»Ð°ÐµÑ‚ Ð½ÐµÐ¿Ð¾Ñ€Ñ‚Ð°Ð±ÐµÐ»ÑŒÐ½Ñ‹Ð¼Ð¸ ÐºÐ¾Ð¼Ð¼ÐµÐ½Ñ‚Ð°Ñ€Ð¸ ÑÐ¾ ÑÐ²Ð¾Ð¸Ð¼  Win-1251*/
 #include "NumericalMethodsMath.h"
-int iterations; //÷èñëî èòåðàöèé â normExp
-clock_t globalT; //ãëîáàëüíàÿ ñòðóêòóðà âðåìåíè
-double wastedTime; // ãëîàáëüíàÿ ïåðåìåííàÿ, óêàçûâþùàÿ âðåìÿ ìåæäó âûçîâàìè startClock è stopClock;
+int iterations; //Ã·Ã¨Ã±Ã«Ã® Ã¨Ã²Ã¥Ã°Ã Ã¶Ã¨Ã© Ã¢ normExp
+clock_t globalT; //Ã£Ã«Ã®Ã¡Ã Ã«Ã¼Ã­Ã Ã¿ Ã±Ã²Ã°Ã³ÃªÃ²Ã³Ã°Ã  Ã¢Ã°Ã¥Ã¬Ã¥Ã­Ã¨
+double wastedTime; // Ã£Ã«Ã®Ã Ã¡Ã«Ã¼Ã­Ã Ã¿ Ã¯Ã¥Ã°Ã¥Ã¬Ã¥Ã­Ã­Ã Ã¿, Ã³ÃªÃ Ã§Ã»Ã¢Ã¾Ã¹Ã Ã¿ Ã¢Ã°Ã¥Ã¬Ã¿ Ã¬Ã¥Ã¦Ã¤Ã³ Ã¢Ã»Ã§Ã®Ã¢Ã Ã¬Ã¨ startClock Ã¨ stopClock;
+long dummy; //Ð´Ð»Ñ Ñ„ÑƒÐ½ÐºÑ†Ð¸Ð¸ rnd();
 
 void  NumMet::startClock(void) {		//http://www.cplusplus.com/reference/ctime/clock/
 	 ::globalT = clock();
@@ -14,7 +15,7 @@ void  NumMet::stopClock(void) {		//http://www.cplusplus.com/reference/ctime/cloc
 double   NumMet::bydloExp(double x) {
 	double exp = 0;
 	if (x >= 0) {
-		for (int k = 0; k <= 170; k++) { //ïîñëå 170 åñòü ðèñê ïåðåïîëíèòü double ïðè âû÷èñëåíèè ôàêòîðèàëà 
+		for (int k = 0; k <= 170; k++) { //Ã¯Ã®Ã±Ã«Ã¥ 170 Ã¥Ã±Ã²Ã¼ Ã°Ã¨Ã±Ãª Ã¯Ã¥Ã°Ã¥Ã¯Ã®Ã«Ã­Ã¨Ã²Ã¼ double Ã¯Ã°Ã¨ Ã¢Ã»Ã·Ã¨Ã±Ã«Ã¥Ã­Ã¨Ã¨ Ã´Ã ÃªÃ²Ã®Ã°Ã¨Ã Ã«Ã  
 			exp += pow(x, (double)k) / NumMet::factorial(k);
 		}
 	}
@@ -45,7 +46,7 @@ double   NumMet::normExp(double x) {
 	}
 	return exp;
 }
-double  NumMet::bydloCos(double x) { // >30 íà÷èíàåò ñèëüíî îòêëîíÿòüñÿ, äâå öèôðû ïîñëå çàïÿòîé óæå òåðÿþòñÿ
+double  NumMet::bydloCos(double x) { // >30 Ã­Ã Ã·Ã¨Ã­Ã Ã¥Ã² Ã±Ã¨Ã«Ã¼Ã­Ã® Ã®Ã²ÃªÃ«Ã®Ã­Ã¿Ã²Ã¼Ã±Ã¿, Ã¤Ã¢Ã¥ Ã¶Ã¨Ã´Ã°Ã» Ã¯Ã®Ã±Ã«Ã¥ Ã§Ã Ã¯Ã¿Ã²Ã®Ã© Ã³Ã¦Ã¥ Ã²Ã¥Ã°Ã¿Ã¾Ã²Ã±Ã¿
 	double ans = 0;
 	for (int k = 0; ans != ans + (pow(-1.0, (double)k) * pow((double)x, 2.0*(double)k)) / NumMet::factorial(2.0 * (double)k); k++) {
 		ans += (double)(pow(-1.0, (double)k) * pow((double)x, 2.0*(double)k)) / NumMet::factorial(2.0 * (double)k);
@@ -75,4 +76,125 @@ double   NumMet::periodicSin(double x) {
 		 ::iterations = k;
 	}
 	return ans;
+}
+void NumMet::seed(long d) {
+    ::dummy = d;
+}
+ double NumMet::rnd( ) {   
+    /*L'Ecuyer algorithm for uniform random generator with practically endless  // 
+ 
+// period. Combining 2 sequences. */                        
+ 
+    const long IM1 = 2147483563;
+ 
+    const long IM2 = 2147483399;
+ 
+    const double AM = (1./IM1);
+ 
+    const long IMM1 = (IM1 - 1);
+ 
+    const int IA1 = 40014;
+ 
+    const int IA2 = 40692;
+ 
+    const long IQ1 = 53668;
+ 
+    const long IQ2 = 52774;
+ 
+    const int IR1 = 12211;
+ 
+    const int IR2 = 3791;
+ 
+    const int NTAB = 32;
+ 
+    const int NWUP = 8;
+ 
+    const double NDIV = (1 + IMM1 / NTAB);
+ 
+    const double EPS = 1.2e-7;
+ 
+    const double RNMX = (1.0 - EPS);
+ 
+    
+ 
+    static long dummy2 = 123456789;
+ 
+    static long iy = 0;
+ 
+    static long iv[32];
+ 
+ 
+ 
+    long k = 0;
+ 
+ 
+ 
+    // initialize the random sequence (first set of coefficients, the 
+ 
+    // routine close to that in the function above
+ 
+    if (::dummy <= 0 || !iy)
+ 
+    {
+ 
+        // avoid negative or zero seed
+ 
+        if (::dummy < 0)
+ 
+            ::dummy =- ::dummy;
+ 
+        else
+ 
+            if (!::dummy)
+ 
+                ::dummy = 1;
+ 
+        dummy2 = ::dummy;
+ 
+        // after NWUP warmups, initialize shuffle table
+ 
+        for (int j = NTAB + NWUP - 1; j >= 0; --j)
+ 
+        {
+ 
+            k = ::dummy / IQ1;
+ 
+            if ((::dummy = IA1 * (::dummy - k * IQ1) - IR1 * k) < 0) ::dummy += IM1;
+ 
+            if (j < NTAB) iv[j] = ::dummy;
+ 
+        }
+ 
+        // first specimen from the table
+ 
+        iy = iv[0];
+ 
+    }
+ 
+    // regular work: generate 2 sequences.
+ 
+    k = ::dummy / IQ1;
+ 
+    if ((::dummy = IA1 * (::dummy - k * IQ1) - IR1 * k) < 0) ::dummy+=IM1;
+ 
+    k = dummy2 / IQ2;
+ 
+    if ((dummy2 = IA2 * (dummy2 - k * IQ2) - IR2 * k) < 0) dummy2+=IM2;
+ 
+    // shuffle output combining 2 sequences
+ 
+    int j = 0;
+ 
+    iy = iv[j = iy / NDIV] - dummy2;
+ 
+    iv[j] = ::dummy;
+ 
+    if(iy < 1) iy += IMM1;
+ 
+    // return the result, as in the previous function
+ 
+    double temp;
+ 
+    return (double)(((temp = AM * iy) > RNMX ? RNMX : temp));
+ 
 }
