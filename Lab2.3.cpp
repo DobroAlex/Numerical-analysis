e68fd709b54056ea26d7d3df87847c8a7e95e684
Alex <alexkoshernosiegov@gmail.com>
2017-11-04 21:20:44 +0400

    Дописана, проверяется на правильность

--- Lab2.3.cpp
+++ Lab2.3.cpp
@@ -35,6 +35,7 @@ int main(int argc, char** argv) {
     getchar();
     int n[2] = {10,20};
     cout<<"\n\n\n A:f(x) = |x|\n\n\n";
+    getchar();
     for (int j = 0; j < 2; j++) {
         double  * X = new double [n[j]+1];
         double  * realF = new double [n[j]+1];
@@ -48,13 +49,54 @@ int main(int argc, char** argv) {
             cout<<"\nn = " << n[j] << "\ti = " << i << "\tX = "<<X[i]<<"\tF(x) = " << realF[i]<<"\tLn(x) = " << NumMet::lagranz(X[i], X, realF, n[j]+1) << "\tNn(x) = " << NumMet::Newton(X[i], n[j]+1, X, realF, X[1]-X[0])<<endl; 
         }
         cout <<"\nЭкстарполяция:\n";
-        for (double test = -10.; test != 20.; test+= 5.) {
-            cout << "\nn = " << n[j] << "\tx= " << test << "\tF(x) = " <<  NumMet::abs(test) <<"\tLn(x) = " << NumMet::lagranz(test, X, realF, n[j]+1) << "\tNn(x) = " << NumMet::Newton(test, n[j]+1, X, realF, 5.)<<endl; 
+        for (double test = -1./(X[1]-X[0]); test <= 1./(X[1]-X[0]); test += (X[1]-X[0]) ) {
+            cout << "\nn = " << n[j] << "\tx= " << test << "\tF(x) = " <<  NumMet::abs(test) <<"\tLn(x) = " << NumMet::lagranz(test, X, realF, n[j]+1) << "\tNn(x) = " << NumMet::Newton(test, n[j]+1, X, realF, X[1]-X[0])<<endl; 
         }
         delete [] X;
         delete [] realF;
     }
+    cout << "\n\n\nB:f(x) = x^3";
     getchar();
+    for (int j = 0; j < 2; j++) {
+        double  * X = new double [n[j]+1];
+        double  * realF = new double [n[j]+1];
+        for (int i = 0; i <= n[j]; i++) {
+            double h = 2./n[j];
+            X[i] = -1. + i * h;
+            realF[i] = pow(X[i], 3.);
+        }
+        cout << "Интерполяция:";
+        for (int  i = 0; i <= n[j]; i++) {
+            cout<<"\nn = " << n[j] << "\ti = " << i << "\tX = "<<X[i]<<"\tF(x) = " << realF[i]<<"\tLn(x) = " << NumMet::lagranz(X[i], X, realF, n[j]+1) << "\tNn(x) = " << NumMet::Newton(X[i], n[j]+1, X, realF, X[1]-X[0])<<endl; 
+        }
+        cout <<"\nЭкстарполяция:\n";
+        for (double test = -1./(X[1]-X[0]); test <= 1./(X[1]-X[0]); test += (X[1]-X[0]) ) {
+            cout << "\nn = " << n[j] << "\tx= " << test << "\tF(x) = " <<  NumMet::abs(test) <<"\tLn(x) = " << NumMet::lagranz(test, X, realF, n[j]+1) << "\tNn(x) = " << NumMet::Newton(test, n[j]+1, X, realF, X[1]-X[0])<<endl; 
+        }
+        delete [] X;
+        delete [] realF;
+    }
+    cout << "\n\n\nC:f(x) = cos(PI*x/2)";
+    getchar();
+    for (int j = 0; j < 2; j++) {
+        double  * X = new double [n[j]+1];
+        double  * realF = new double [n[j]+1];
+        for (int i = 0; i <= n[j]; i++) {
+            double h = 2./n[j];
+            X[i] = -1. + i * h;
+            realF[i] = cos(MY_PI*X[i]/2.);
+        }
+        cout << "Интерполяция:";
+        for (int  i = 0; i <= n[j]; i++) {
+            cout<<"\nn = " << n[j] << "\ti = " << i << "\tX = "<<X[i]<<"\tF(x) = " << realF[i]<<"\tLn(x) = " << NumMet::lagranz(X[i], X, realF, n[j]+1) << "\tNn(x) = " << NumMet::Newton(X[i], n[j]+1, X, realF, X[1]-X[0])<<endl; 
+        }
+        cout <<"\nЭкстарполяция:\n";
+        for (double test = -1./(X[1]-X[0]); test <= 1./(X[1]-X[0]); test += (X[1]-X[0]) ) {
+            cout << "\nn = " << n[j] << "\tx= " << test << "\tF(x) = " <<  NumMet::abs(test) <<"\tLn(x) = " << NumMet::lagranz(test, X, realF, n[j]+1) << "\tNn(x) = " << NumMet::Newton(test, n[j]+1, X, realF, X[1]-X[0])<<endl; 
+        }
+        delete [] X;
+        delete [] realF;
+    }
     return 0;
 }
 
