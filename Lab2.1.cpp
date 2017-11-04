866b1261fc85dc5750baf212f46280173dee9393
Alex <alexkoshernosiegov@gmail.com>
2017-11-04 15:32:42 +0400

    Переписан Чебышев и равномерное разбиение, меньше фуфла

--- Lab2.1.cpp
+++ Lab2.1.cpp
@@ -19,7 +19,6 @@ using NumMet::lenBetweenpoints;
 /*int lenBetweenpoints (int x1, int x2);
 double lagranz(double t, double * X, double *Y, double n  ); //интерполяционный многочлен */
 double testFunc(double x); /*Функция, которую интерполируем */
-double Chebyshev (int n , double t ) ; //многочлен Чебышева n-ого рода,  -1<=t<=1
 int main(int argc, char** argv) {
     /*Interpolation corectness test*/
     double OX[5] = {-1.0, -.5, .0, .5, 1.0};
@@ -65,23 +64,17 @@ int main(int argc, char** argv) {
     /*f(x) = 1/(1+d*x^2), x [-1;1], d = 10+k;*/
     /*Part (a)*/
     int n[] = {4, 10, 20};
+    cout <<"\n\n\nРавномерное разбиение\n\n\n";
     for (int j = 0; j < (int)(sizeof(n)/sizeof(n[0])); j++) {
         double  * realF = new double [n[j]];
         double * X = new double [n[j]];
-        double ansX = -1.;
-        double len = ::lenBetweenpoints(-1,1);
-        double delta  = (double)::lenBetweenpoints(-1, 1) / (double)(n[j]);
-        X[0] = -1.;
-        for (int i = 1 ; i < n[j]; i++) {
-            X[i] = X[i-1] + delta;    
-            double h = 2.0 / n[j];
-        //cout<<endl<<endl;
-            realF[i] = 1.0/(1.0 + (10+MY_VAR)* X[i] * X[i]);
-            cout<<"n = " <<n[j] << " i = " << i <<" X[i] = " << X[i] << " realF[i] = " << realF[i] << endl;
+        for (int i = 0; i < n[j];i++) {
+            double h = 2./n[j];
+            X[i] = -1. + i *h;
+            realF[i] = 1./(1. + (10 + MY_VAR)* pow(X[i], 2.));
         }
-        cout<<"Into/Extropolation:\n";
-        for (double test = -2.;test <= 2.; test+=delta ){
-            cout<<"X = "<<test<<"\tReal F(x) = " <<  1.0/(1.0 + (10+MY_VAR)* test * test) << "\tLn(x) = "<<::lagranz(test, X, realF, n[j])<<endl;
+        for (int  i = 0; i < n[j]; i++) {
+            cout <<"\n n = " << n[j] << "\t X=" << X[i] << "\t F(x) = " << realF[i] << "\t Ln(x) =" << lagranz(X[i], X, realF, n[j]);
         }
         cout << "\n\n\n";
        delete [] X;
@@ -108,7 +101,4 @@ int main(int argc, char** argv) {
 
 inline double testFunc(double x) {
 	return  2.0 * x*x*x*x + 3.0 * x*x*x + 4.0*x*x + x + 5.0; // for example
-}
-inline double Chebyshev (int n , double t ) { //многочлен Чебышева n-ого рода,  -1<=t<=1
-    return cos ( (double)n * acos(t)); //http://www.intuit.ru/studies/courses/1012/168/lecture/4600?page=5
 }
